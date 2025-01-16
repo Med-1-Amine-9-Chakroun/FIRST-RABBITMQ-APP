cf26f40e42cf5fcd564c753760f35d1638a82055
Med-1-Amine-9-Chakroun <chakrounmedamine19@gmail.com>
2025-01-16 21:25:50 +0100

    consumers and producer created with fanout exchange method (pub/sub)

--- producer.py
+++ producer.py
@@ -1,24 +1,18 @@
 import pika
-import time
-import random
+from pika.exchange_type import ExchangeType
+
 connection_parameters = pika.ConnectionParameters("localhost")
 
 connection = pika.BlockingConnection(connection_parameters)
 
 channel = connection.channel()
 
-channel.queue_declare(queue="letterbox")
+channel.exchange_declare(exchange='pubsup', exchange_type=ExchangeType.fanout)
 
-messageId = 1
+message = "hello, i want to broadcast this message"
 
-while(True):
-    message = f"hello, this is my {messageId} message"
+channel.basic_publish(exchange='pubsup', routing_key='letterbox', body=message)
 
-    channel.basic_publish(exchange='', routing_key='letterbox', body=message)
+print("sent message: ",message)    
 
-    print("sent message: ",message)
-    
-    time.sleep(random.randint(1, 4))
-
-    messageId += 1
-    
+connection.close()
---
+++ secondconsumer.py
@@ -0,0 +1,24 @@
+import pika
+from pika.exchange_type import ExchangeType
+
+def on_message_received(ch, method, propreties, body):    
+    print('Secondconsumer : received new message', body)
+    
+
+connection_parameters = pika.ConnectionParameters("localhost")
+
+connection = pika.BlockingConnection(connection_parameters) 
+
+channel = connection.channel()
+
+channel.exchange_declare(exchange='pubsup', exchange_type=ExchangeType.fanout)
+
+queue = channel.queue_declare(queue="", exclusive=True)
+
+channel.queue_bind(exchange='pubsup', queue=queue.method.queue)
+
+channel.basic_consume(queue=queue.method.queue, auto_ack=True, on_message_callback=on_message_received)
+
+print("Start  consuming")
+
+channel.start_consuming()   
