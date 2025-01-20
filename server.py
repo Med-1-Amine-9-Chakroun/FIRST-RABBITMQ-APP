d25b3fff241541b8eb948e457f1617c3f735a2ea
Med-1-Amine-9-Chakroun <chakrounmedamine19@gmail.com>
2025-01-20 21:27:29 +0100

    server updated

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import pika
 from pika.exchange_type import ExchangeType
 
-def on_message_received(ch, method, propreties, body):    
-    print('received new message : ', body)
+def on_request_message_received(ch, method, propreties, body):    
+    print('Request received : ', propreties.correlation_id)
+    ch.basic_publish('', routing_key = propreties.reply_to,
+                     body=f'hey its your reply to{propreties.correlation_id}')
     
 
 connection_parameters = pika.ConnectionParameters("localhost")
@@ -11,12 +13,10 @@ connection = pika.BlockingConnection(connection_parameters)
 
 channel = connection.channel()
 
-channel.queue_declare(queue="letterbox")
+channel.queue_declare(queue="request-queue")
 
-queue = channel.queue_declare(queue="", exclusive=True)
+channel.basic_consume(queue="request-queue", auto_ack=True, on_message_callback=on_request_message_received)
 
-channel.basic_consume(queue="letterbox", auto_ack=True, on_message_callback=on_message_received)
-
-print("Start  consuming")
+print("Starting Server")
 
 channel.start_consuming()   
